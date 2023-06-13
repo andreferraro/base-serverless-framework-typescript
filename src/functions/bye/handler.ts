9279dea7fcadd5f37a0d4cbb493096b0e2752386
Andre Ferraro <aferrarobr@gmail.com>
2023-06-13 20:23:41 -0300

    feat: add sample POST endpoint

---
+++ src/functions/bye/handler.ts
@@ -0,0 +1,21 @@
+import { formatJSONResponse } from '@libs/api-gateway';
+import { middyfy } from '@libs/lambda';
+
+import { generateRandomNumber } from './handler.service';
+import schema from './schema';
+
+import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
+export const main: ValidatedEventAPIGatewayProxyEvent<
+    typeof schema
+> = async event => {
+    const name = event.body
+        ? event.body.name
+        : 'World';
+    const randomNumber = generateRandomNumber();
+    return formatJSONResponse({
+        message: `Bye ${name}. Your random number: ${randomNumber}!`,
+        randomNumber
+    });
+};
+
+export const bye = middyfy(main);
